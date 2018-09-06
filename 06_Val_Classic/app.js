b590ab07eba4924ade5f1eab87a302f702fc1a1e
Francisco <pulin@hotmail.com>
2018-09-06 22:59:01 +0200

    05-06 desde casa

--- 06_Val_Classic/app.js
+++ 06_Val_Classic/app.js
@@ -5,20 +5,33 @@ function main() {
 
     // focus / blur
 
-    inNombre.addEventListener('blur', validaTexto)
+    inNombre.addEventListener('blur', validaTextoblur)
     inNombre.addEventListener('input', validaTexto)
-    inApellido.addEventListener('blur', validaTexto)
+    inApellido.addEventListener('blur', validaTextoblur)
     inApellido.addEventListener('input',validaTexto)
+    
+    function validaTextoblur() {
+        if(!this.value) {
+            console.log(`${this.id} incorrecto_blur`)
+            console.dir(this)
+            this.nextElementSibling.innerHTML = `${this.id} incorrecto_blur`
+            btnEnviar.disabled = true
+        } else {
+            this.nextElementSibling.innerHTML = 'okblur'
+            validaFormulario()
+        }
+    }    
     
     function validaTexto() {
         if(!this.value) {
-            console.log(`${this.id} incorrecto`)
+            console.log(`${this.id} incorrecto_input`)
             console.dir(this)
-            this.nextElementSibling.innerHTML = `${this.id} incorrecto`
+            this.nextElementSibling.innerHTML = `${this.id} incorrecto_input`
+            btnEnviar.disabled = true
         } else {
-            this.nextElementSibling.innerHTML = ''
+            this.nextElementSibling.innerHTML = 'ok'
             validaFormulario()
-        }
+        }   
     }
 
     function validaFormulario () {
