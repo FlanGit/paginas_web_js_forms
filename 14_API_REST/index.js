2c07b08f5a762ab44b98105933781e5476f9dd4f
Francisco <pulin@hotmail.com>
2018-09-18 13:58:10 +0200

    14 api rest

--- 14_API_REST/index.js
+++ 14_API_REST/index.js
@@ -1,8 +1,8 @@
 import { ListaTareas } from "./lista-tareas.js";
 
 export class Index {
-    constructor () {
-        console.log('Creado Index')
+    constructor() {
         new ListaTareas()
     }
+    
 }
--- 14_API_REST/lista-tareas.js
+++ 14_API_REST/lista-tareas.js
@@ -1,26 +1,67 @@
 import { FetchService } from "./fetch-service.js";
-import { Tarea } from "./tarea.js"
+
 export class ListaTareas {
     constructor() {
-        this.uRL = 'http://localhost:3000/tareas' // url del servidor de la base de datos
-        this.aTareas = []
         this.nodoListaTareas = document.querySelector('#lista')
+        this.uRL = 'http://localhost:3000/tareas'
+        this.aTareas = []
+        this.fetchService = new FetchService()
         this.getTareas()
     }
-    
+
     getTareas() {
-        new FetchService({url:this.uRL, method: 'GET'})
-            .get()
-            .then ( data => {
+        this.fetchService.send(this.uRL, {method: 'GET' })
+            .then( data => {
                 this.aTareas = data
                 this.renderLista()
-            })
+            },
+            error => {console.dir(error)}
+            )
     }
+    
     renderLista() {
         let html = ''
-        this.aTareas.forEach( 
-            item => html += new Tarea(item).renderTarea()
+        this.aTareas.forEach(
+            item =>  html += this.renderTarea(item)
         )
         this.nodoListaTareas.innerHTML = html
+        this.aNodosChecks = document.querySelectorAll('[name="is-completa"]')
+        this.aNodosBorrar = document.querySelectorAll('.borrar-tarea')
+        this.aNodosChecks.forEach(
+            item => item.addEventListener('change', this.checkTarea.bind(this))
+        )
+        this.aNodosBorrar.forEach(
+            item => item.addEventListener('click', this.borrarTarea.bind(this))
+        )
     }
+
+    renderTarea(data) {
+        let htmlView =  `
+            <li>
+            <input type="checkbox" name="is-completa" id="check-${data.id}"
+                data-id="${data.id}" ${data.isComplete ? 'checked' : '' }>
+            <span class="nombre-tarea">${data.name}</span>
+            <span id="borrar-${data.id}" data-id="${data.id}"
+                class="borrar-tarea">🗑️</span>
+            </li>
+        `
+        return htmlView
+    }
+
+    checkTarea(oEv) {
+        console.log(oEv.target.dataset.id)
+    }
+
+    borrarTarea(oEv) {
+        console.log(oEv.target.dataset.id)
+        // TODO Borar en Servicio Web
+        let url = this.uRL + '/' + oEv.target.dataset.id
+        this.fetchService.send(url, {method: 'DELETE' })
+            .then(
+                data => console.log(data),
+                error => console.log(error)
+            )
+        this.getTareas()
+    }
+
 }
