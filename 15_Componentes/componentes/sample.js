ad8710c390ff564a3ccd59402f05b43e40803ace
Francisco <pulin@hotmail.com>
2018-09-21 14:02:55 +0200

    15componentes

---
+++ 15_Componentes/componentes/sample.js
@@ -0,0 +1,16 @@
+export class sample{
+
+    constructor(){
+
+    }
+
+    render(){
+        return
+    }
+
+
+
+
+
+
+}
---
+++ 15_Componentes/pages/about.js
@@ -0,0 +1,10 @@
+import { PageController } from "../page.controller";
+
+class About extends PageController {
+    constructor() {
+        this.title = 'Componentes - About'
+        this.contentFooter = {
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => { new About0000000000000000000000000000000()})
