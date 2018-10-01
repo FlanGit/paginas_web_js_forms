03a2d3679fde93d1592723e93aacc767f68d9cba
Francisco <pulin@hotmail.com>
2018-10-01 21:33:50 +0200

    new angular

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CoreModule } from './core/core.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { HomeModule } from './home/home.module';
+import { TareasModule } from './tareas/tareas.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CoreModule,
+    ContactsModule,
+    HomeModule,
+    TareasModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+}
