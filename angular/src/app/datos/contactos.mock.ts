09c0171c937d0ddae84e67bc5cb3ccbd89f4a6f7
Francisco <pulin@hotmail.com>
2018-10-03 13:56:48 +0200

    borra padre e hijo

---
+++ angular/src/app/datos/contactos.mock.ts
@@ -0,0 +1,109 @@
+import {ContactoIf  } from "../models/contacto.model";
+
+export const USERS:Array<ContactoIf> =  [
+    {
+    gender: "male",
+    name: {
+    title: "mr",
+    first: "luke",
+    last: "morenito"
+    },
+    location: {
+    street: "5398 robinson rd",
+    city: "anchorage",
+    state: "north dakota",
+    postcode: 72999,
+    coordinates: {
+    latitude: "84.1723",
+    longitude: "-111.1245"
+    },
+    timezone: {
+    offset: "-10:00",
+    description: "Hawaii"
+    }
+    },
+    email: "luke.moreno@example.com",
+    login: {
+        uuid: "a918b0ab-b5cb-44cd-8a81-4b84a5fb981b",
+        username: "orangebird344",
+        password: "nintendo",
+        salt: "TtVSpUZs",
+        md5: "03665c644ab5acd2af8013121ef7b0bc",
+        sha1: "5e55a9aa4f2cc7cd458e1b16ae76b8ad5b0e3009",
+        sha256: "242f4ed7a6d1ffac4ba069708953c99cafd74369e43064493a4d63429fa676ce"
+        },
+    dob: {
+        date: "1981-11-23T15:30:06Z",
+        age: 36
+        },
+    registered: {
+        date: "2010-10-08T17:17:31Z",
+        age: 7
+        },
+    phone: "(639)-765-0779",
+    cell: "(114)-124-5322",
+    id: {
+    name: "SSN",
+    value: "076-79-9640"
+    },
+    picture: {
+    large: "https://randomuser.me/api/portraits/men/27.jpg",
+    medium: "https://randomuser.me/api/portraits/med/men/27.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/men/27.jpg"
+    },
+    nat: "US"
+    }, 
+
+    {
+        gender: "male",
+        name: {
+        title: "mr",
+        first: "francisca ",
+        last: " Rodriguez"
+        },
+        location: {
+        street: "5398 robinson rd",
+        city: "anchorage",
+        state: "north dakota",
+        postcode: 72999,
+        coordinates: {
+        latitude: "84.1723",
+        longitude: "-111.1245"
+        },
+        timezone: {
+        offset: "-10:00",
+        description: "Hawaii"
+        }
+        },
+        email: "flanflias@hotmail.com",
+        login: {
+            uuid: "a918b0ab-b5cb-44cd-8a81-4b84a5fb981b",
+            username: "orangebird344",
+            password: "nintendo",
+            salt: "TtVSpUZs",
+            md5: "03665c644ab5acd2af8013121ef7b0bc",
+            sha1: "5e55a9aa4f2cc7cd458e1b16ae76b8ad5b0e3009",
+            sha256: "242f4ed7a6d1ffac4ba069708953c99cafd74369e43064493a4d63429fa676ce"
+            },
+        dob: {
+            date: "1964-10-10T15:30:06Z",
+            age: 36
+            },
+        registered: {
+            date: "2010-10-08T17:17:31Z",
+            age: 7
+            },
+        phone: "(809)-565-2611",
+        cell: "(114)-124-5322",
+        id: {
+        name: "SSN",
+        value: "076-79-9640"
+        },
+        picture: {
+        large: "https://randomuser.me/api/portraits/med/women/65.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/65.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/med/women/65.jpg"
+        },
+        nat: "RD"
+        }
+    ] 
