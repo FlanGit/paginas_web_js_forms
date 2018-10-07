4e2cdb8e576d8cb8716be2de22750c4675c5d9a3
Francisco <pulin@hotmail.com>
2018-10-08 00:07:15 +0200

    NuevoAngularMIO

---
+++ angularMIO/src/app/core/cabecera/cabecera.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cas-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
+})
+export class CabeceraComponent implements OnInit {
+  titulo: string
+  fw: string
+  constructor() { }
+
+  ngOnInit() {
+    this.fw = 'Angular'
+    this.titulo = `Proyecto ${this.fw}`
+  }
+
+}
+ 
