708b1075c4289cd9c59581adb7d432c14cbf091e
Francisco <pulin@hotmail.com>
2018-10-10 16:30:34 +0200

    OK

--- angularMIO/src/app/app.module.ts
+++ angularMIO/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -12,17 +20,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     CoreModule,
-
+  /*    ContactsModule,
+        HomeModule,
+        TareasModule, */
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
   ],
   exports: [
 
   ],
   providers: [
+/*     UsuariosService, */
     { provide: LOCALE_ID, useValue: 'es' }
 
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
- 
+  
